fd5e6e7a857676d97fe630c56c07b619586ccd9d
Randy <ransolo@me.com>
2016-12-13 01:41:51 -0600

    update-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ welcome = Welcome.create([{
       <li class='text-center list-group-item list-group-item-action'>And more...</li>
     </ul>",
   notes:
-  "<p class='news-headline'>
+  "<p>
     &nbsp;&nbsp;&nbsp;&nbsp;Our main system of support in gravity is our
     Myofascial system.  This system consists of a continuous web of layered
     collagen fibers that extend in planes from head to toe.  Stress, illness,
